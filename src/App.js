9162b90726b78848cffa5b17d72ca7793847de54
IMRANDIL <aliimranadil2@gmail.com>
2022-05-08 19:58:10 +0530

    yh

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Topbar from './components/topbar/Topbar';
+
+
+function App() {
+  return (
+    <div >
+      <Topbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './Topbar.css'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
+
+
+
+
+
+
+const Topbar = () => {
+    return (
+        <div className='topbar'>
+
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">aiaAdmin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIcons">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Topbar
