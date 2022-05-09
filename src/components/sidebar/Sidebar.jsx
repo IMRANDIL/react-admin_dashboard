e901770e1be5e78200bcb5a03fb652c536b593d6
IMRANDIL <aliimranadil2@gmail.com>
2022-05-09 21:35:38 +0530

    yh

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,16 @@ import './sidebar.css'
 import LineStyleIcon from '@mui/icons-material/LineStyle';
 import InsightsIcon from '@mui/icons-material/Insights';
 import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import PersonIcon from '@mui/icons-material/Person';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import PaidIcon from '@mui/icons-material/Paid';
+import ReportIcon from '@mui/icons-material/Report';
+
+
+
+
+
+
 
 
 
@@ -35,24 +45,66 @@ const Sidebar = () => {
 
 
                 <div className="sidebarMenu">
-                    <h3 className='sidebarTitle'>Dashboard</h3>
+                    <h3 className='sidebarTitle'>Quick Menu</h3>
                     <ul className="sidebarList">
                         <li className="sidebarListItem">
-                            <LineStyleIcon />
+                            <LineStyleIcon className='sidebarIcon' />
                             Home
                         </li>
 
                         <li className="sidebarListItem">
-                            <InsightsIcon />
+                            <InsightsIcon className='sidebarIcon' />
                             Analytics
                         </li>
 
                         <li className="sidebarListItem">
-                            <PointOfSaleIcon />
+                            <PointOfSaleIcon className='sidebarIcon' />
                             Sales
                         </li>
                     </ul>
                 </div>
+
+                <div className="sidebarMenu">
+                    <h3 className='sidebarTitle'>Notifications</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <LineStyleIcon className='sidebarIcon' />
+                            Home
+                        </li>
+
+                        <li className="sidebarListItem">
+                            <InsightsIcon className='sidebarIcon' />
+                            Analytics
+                        </li>
+
+                        <li className="sidebarListItem">
+                            <PointOfSaleIcon className='sidebarIcon' />
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="sidebarMenu">
+                    <h3 className='sidebarTitle'>Staffs</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <LineStyleIcon className='sidebarIcon' />
+                            Home
+                        </li>
+
+                        <li className="sidebarListItem">
+                            <InsightsIcon className='sidebarIcon' />
+                            Analytics
+                        </li>
+
+                        <li className="sidebarListItem">
+                            <PointOfSaleIcon className='sidebarIcon' />
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+
+
             </div>
 
 
