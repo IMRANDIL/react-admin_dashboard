8a31be1ee2141414eea29209f20737cfb9e2dff9
IMRANDIL <aliimranadil2@gmail.com>
2022-05-11 20:26:40 +0530

    yh

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,82 +1,20 @@
-import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from 'react';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import './Chart.css'
 
 
 
 
 
-const Chart = () => {
+const Chart = ({ title, data, dataKey, grid }) => {
 
 
-    const data = [
-        {
-            name: 'Jan',
-            'Active User': 4000,
-
-        },
-        {
-            name: 'Feb',
-            'Active User': 3000,
-
-        },
-        {
-            name: 'March',
-            'Active User': 2000,
-
-        },
-        {
-            name: 'Apr',
-            'Active User': 2780,
-
-        },
-        {
-            name: 'May',
-            'Active User': 1890,
-
-        },
-        {
-            name: 'June',
-            'Active User': 2390,
-
-        },
-        {
-            name: 'July',
-            'Active User': 3490,
-
-        },
-        {
-            name: 'Aug',
-            'Active User': 3090,
-
-        },
-        {
-            name: 'Sept',
-            'Active User': 3590,
-
-        },
-        {
-            name: 'Oct',
-            'Active User': 3990,
-
-        },
-        {
-            name: 'Nov',
-            'Active User': 3190,
-
-        },
-        {
-            name: 'Dec',
-            'Active User': 3790,
-
-        },
-    ];
 
 
 
     return (
         <div className='chart'>
-            <h3 className="chartTitle">User Analytics</h3>
+            <h3 className="chartTitle">{title}</h3>
 
 
             <ResponsiveContainer width='100%' aspect={4 / 1}>
@@ -85,7 +23,8 @@ const Chart = () => {
                     <XAxis dataKey='name' stroke='#5550be' />
                     <Line type='monotone' dataKey='Active User' stroke='#5550be' />
                     <Tooltip />
-                    <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />
+                    {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />}
+
                 </LineChart>
 
             </ResponsiveContainer>
---
+++ src/dummyData.js
@@ -0,0 +1,62 @@
+export const data = [
+    {
+        name: 'Jan',
+        'Active User': 4000,
+
+    },
+    {
+        name: 'Feb',
+        'Active User': 3000,
+
+    },
+    {
+        name: 'March',
+        'Active User': 2000,
+
+    },
+    {
+        name: 'Apr',
+        'Active User': 2780,
+
+    },
+    {
+        name: 'May',
+        'Active User': 1890,
+
+    },
+    {
+        name: 'June',
+        'Active User': 2390,
+
+    },
+    {
+        name: 'July',
+        'Active User': 3490,
+
+    },
+    {
+        name: 'Aug',
+        'Active User': 3090,
+
+    },
+    {
+        name: 'Sept',
+        'Active User': 3590,
+
+    },
+    {
+        name: 'Oct',
+        'Active User': 3990,
+
+    },
+    {
+        name: 'Nov',
+        'Active User': 3190,
+
+    },
+    {
+        name: 'Dec',
+        'Active User': 3790,
+
+    },
+];
