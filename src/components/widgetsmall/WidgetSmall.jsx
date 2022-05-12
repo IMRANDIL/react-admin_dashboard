bbd88e9389a58524c7e9b641fe6ee5457cfbfaa9
IMRANDIL <aliimranadil2@gmail.com>
2022-05-12 20:35:21 +0530

    yh

--- src/components/widgetsmall/WidgetSmall.jsx
+++ src/components/widgetsmall/WidgetSmall.jsx
@@ -7,7 +7,16 @@ import './WidgetSmall.css';
 const WidgetSmall = () => {
     return (
         <div className='widgetSmall'>
-            small
+            <span className="widgetSmTitle">New Joined Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://avatars.githubusercontent.com/u/71559091?s=400&u=19775acdc2698063179841ec84602507b5d6e332&v=4" alt="widgetSmImg" className="widgetSmimg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Ali Imran Adil</span>
+                        <span className="widgetSmUserJobTitle">Software Engineer</span>
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
